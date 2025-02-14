88507df9e40fcb5083817c56d3196991863a7054
nautilus@gideboctopus <nautilus@trovaprezzi.it>
2025-02-14 15:46:31 +0100

    fix

--- app/redis_client_base.rb
+++ app/redis_client_base.rb
@@ -3,9 +3,14 @@ require('redis')
 class RedisClientBase
   def initialize
     @client = Redis.new(host: '192.168.56.11', port: 6379)
+    delete_all
   end
 
   def close_connection
     @client.close
+  end
+
+  def delete_all
+    @client.flushall
   end
 end
---
+++ app/timeseries_string.rb
@@ -0,0 +1,49 @@
+require_relative('redis_client_base')
+
+class TimeseriesString < RedisClientBase
+
+  def initialize(namespace)
+    @namespace = namespace
+    super()
+  end
+
+  def insert(timestamp_in_seconds)
+    key = key_name(timestamp_in_seconds)
+    @client.incr(key)
+
+    one_hour_in_seconds = 1 * 60 * 60
+    @client.expire(key, one_hour_in_seconds)
+  end
+
+  def fetch(start_second, end_second)
+    seconds = (start_second..end_second).to_a
+    keys = seconds.map { |second| key_name(second) }
+
+    keys.each do|key|
+      total = @client.get(key)
+      value = total.nil? ? 0 : total
+      puts("At #{key} the visits was: #{value}")
+    end
+  end
+
+  def key_name(timestamp_in_seconds)
+    "#{@namespace}:second:#{timestamp_in_seconds}"
+  end
+end
+
+ts = TimeseriesString.new('money')
+1.times { ts.insert(0) }
+3.times { ts.insert(1) }
+1.times { ts.insert(2) }
+4.times { ts.insert(3) }
+5.times { ts.insert(4) }
+7.times { ts.insert(5) }
+1.times { ts.insert(6) }
+2.times { ts.insert(7) }
+0.times { ts.insert(8) }
+0.times { ts.insert(9) }
+9.times { ts.insert(10) }
+3.times { ts.insert(11) }
+
+ts.fetch(1, 10)
+ts.close_connection
