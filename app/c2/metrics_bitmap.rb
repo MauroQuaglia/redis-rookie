e1a8646c213cff37b444ad0635f5099d1241c638
nautilus@gideboctopus <nautilus@trovaprezzi.it>
2025-02-14 11:47:58 +0100

    Fix

--- app/c2/metrics_bitmap.rb
+++ app/c2/metrics_bitmap.rb
@@ -12,11 +12,31 @@ class MetricsBitmap < RedisClientBase
     key = "visits:daily:#{date}"
     count = @client.bitcount(key)
     puts("#{date} had #{count} visits.")
-    #bitpos mauro 1 0 -1 BIT
-    @client.bitpos()
+  end
+
+  def show_user_ids_from_visit(date)
+    key = "visits:daily:#{date}"
+    ids = []
+    start = 0
+    while (start >= 0)
+      id = @client.bitpos(key, 1, start, -1, scale: 'BIT')
+      if id != -1
+        ids << id
+        start = id + 1
+      else
+        break
+      end
+    end
+    puts("Users #{ids.join(',')} visited on #{date}")
   end
 end
 
 mb = MetricsBitmap.new
+mb.store_daily_visit('2015-01-01', '1')
+mb.store_daily_visit('2015-01-01', '2')
+mb.store_daily_visit('2015-01-01', '10')
+mb.store_daily_visit('2015-01-01', '55')
 
+mb.count_visits('2015-01-01')
+mb.show_user_ids_from_visit('2015-01-01')
 mb.close_connection
