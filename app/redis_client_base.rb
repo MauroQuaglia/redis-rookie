e85a83b37370fb9f4be4865bcdaeb2f3270cfd46
nautilus@gideboctopus <nautilus@trovaprezzi.it>
2025-02-28 14:48:52 +0100

    sentinel

--- app/redis_client_base.rb
+++ app/redis_client_base.rb
@@ -1,8 +1,8 @@
 require('redis')
 
 class RedisClientBase
-  def initialize
-    @client = Redis.new(host: '192.168.56.11', port: 6379, password: 'foobar')
+  def initialize(sentinel: true)
+    @client = sentinel ? client_with_sentinel : client_without_sentinel
     delete_all
   end
 
@@ -12,5 +12,28 @@ class RedisClientBase
 
   def delete_all
     @client.flushall
+  end
+
+  private
+
+  def client_with_sentinel
+    sentinels = [{ host: "192.168.56.11", port: 26379 }]
+
+    @client = Redis.new(
+      sentinels: sentinels,
+      role: :master,
+      password: 'foobar',
+      name: "mymaster"
+    )
+    # Viene chiamato Sentinel che gli ritorna come chiamare Redis!
+    # raise @client.inspect è illuminante: <Redis client v5.4.0 for redis://127.0.0.1:6379> (RuntimeError)
+    # Quindi devo aprire da Vagrant la porta 6379 altrimenti non lo contatterò mai!
+  end
+
+  def client_without_sentinel
+    Redis.new(
+      host: '192.168.56.11',
+      port: 6379,
+      password: 'foobar')
   end
 end
