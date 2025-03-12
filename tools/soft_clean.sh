f3a48557d7ff8cd4c02255b0443c7b6db4cec578
nautilus@gideboctopus <nautilus@trovaprezzi.it>
2025-03-12 08:58:40 +0100

    Fix delle prove

--- tools/soft_clean.sh
+++ tools/soft_clean.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 echo "Clean Docker for Redis..."
-docker container stop redis-insight-redisinsight-1
+docker container stop docker-redisinsight-1 docker-redis7-1
 docker container prune -f
 docker image prune -af
 docker volume prune -af
